0f15a53b0fb876241a0eb5e4d6a83993fccb7813
felipe.bignoto <felipe.bignoto@open-co.com>
2025-05-31 16:42:14 -0300

    feat: Adiciona classe ResourceNotFoundException para tratamento padronizado de recursos não encontrados

---
+++ src/main/java/com/exemplo/api/common/ResourceNotFoundException.java
@@ -0,0 +1,14 @@
+package com.exemplo.api.common;
+
+/**
+ * Exceção personalizada para recursos não encontrados
+ */
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+
+    public ResourceNotFoundException(String resourceName, String fieldName, Object fieldValue) {
+        super(String.format("%s não encontrado com %s: '%s'", resourceName, fieldName, fieldValue));
+    }
+}
